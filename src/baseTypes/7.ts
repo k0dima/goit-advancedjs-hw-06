11860ec6c63ea42c8c10829023061c3d68320d45
k0dima <214964089+k0dima@users.noreply.github.com>
2026-05-15 16:06:42 +0700

    feat: implement type-safe assignments and base type constraints for homework exercises

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfWeek {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+  return day === DayOfWeek.SATURDAY || day === DayOfWeek.SUNDAY;
+}
+
+export { };
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,20 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface IComponentProps {
+  title: string;
+}
+
+class Component<T extends object> {
+  constructor(public props: T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IComponentProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+export { };
